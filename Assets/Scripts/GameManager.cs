f1a6ba46f6e06828d28b1c34be7c7eae4edad711
mickey kawick <mkawick@yahoo.com>
2020-06-23 23:40:14 +0100

    stubs only

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    TMP_Text text;
+    public Button questionButton;
+    public Button[] buttons;
+    public Color newColor;
+    private Dictionary<int, TriviaQuestion> questionMap;
+
+    private void Awake()
+    {
+        questionMap = new Dictionary<int, TriviaQuestion>();
+        var questions = Resources.FindObjectsOfTypeAll<TriviaQuestion>();
+        for(int i=0; i< questions.Length; i++)
+        {
+            questionMap.Add(questions[i].GetInstanceID(), questions[i]);
+        }
+
+        /*TextMeshPro textmeshPro = GetComponent<TextMeshPro>();
+        textmeshPro.color = new Color32(255, 128, 0, 255);*/
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TriviaQuestion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TriviaQuestionData", menuName = "ScriptableObjects/TriviaQuestion", order = 1)]
+public class TriviaQuestion : ScriptableObject
+{
+    public string question;
+
+    public int levelOfDifficulty;
+    public int numOptions;
+    public int numAnswersNeeded;
+    public string[] correctAnswers;
+    public string[] falseAnswers;
+}
