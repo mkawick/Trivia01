c75962891cead881f6c39f80991c18efcd85ae97
mickey kawick <mkawick@yahoo.com>
2020-06-24 00:07:57 +0100

    minimal working version

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -24,11 +24,67 @@ public class GameManager : MonoBehaviour
         /*TextMeshPro textmeshPro = GetComponent<TextMeshPro>();
         textmeshPro.color = new Color32(255, 128, 0, 255);*/
     }
+
+    public int GetQuestion(int random)
+    {
+        return questionMap.ElementAt(random).Key;
+    }
     void Start()
     {
+        int selection = Random.Range(0, questionMap.Count);
+        int whichQuestion = GetQuestion(selection);
+
+        TriviaQuestion tq = questionMap[whichQuestion];
+        questionButton.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = tq.question;
+
         
+        var buttonList = PutAllButtonsInAList(tq.numOptions);
+        var correctAnswers = PutOptionsInList(tq.correctAnswers);
+        var falseAnswers = PutOptionsInList(tq.falseAnswers);
+        // todo: hide unused buttons
+        for (int i= 0; i< tq.numAnswersNeeded; i++)
+        {
+            Button b = buttonList[Random.Range(0, buttonList.Count)];
+            string answer = correctAnswers[Random.Range(0, correctAnswers.Count)];
+
+            b.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = answer;
+
+            buttonList.Remove(b);
+            correctAnswers.Remove(answer);
+        }
+        int numFalseAnswersNeeded = tq.numOptions - tq.numAnswersNeeded;
+        for (int i = 0; i < numFalseAnswersNeeded; i++)
+        {
+            Button b = buttonList[Random.Range(0, buttonList.Count)];
+            string answer = falseAnswers[Random.Range(0, falseAnswers.Count)];
+
+            b.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = answer;
+
+            buttonList.Remove(b);
+            falseAnswers.Remove(answer);
+        }
     }
 
+    List<Button> PutAllButtonsInAList(int num)
+    {
+        if (num == buttons.Length)
+        {
+            return buttons.ToList();
+        }
+        else
+        {
+            List<Button> tempList = new List<Button>();
+            for(int i=0; i<num; i++)
+            {
+                tempList.Add(buttons[i]);
+            }
+            return tempList;
+        }
+    }
+    List<string> PutOptionsInList(string[] options)
+    {
+        return options.ToList();
+    }
     // Update is called once per frame
     void Update()
     {
